7b63b450c643af488320f0b9428ee4f1728c922c
Cristian Ortiz Rodríguez <104893717+cristianortizrodriguez@users.noreply.github.com>
2022-05-26 18:39:12 +0200

    archivo.js

---
+++ ejercicios_js1/archivo.js
@@ -0,0 +1,15 @@
+<script>hola mundo</script> 
+<noscript>Lo siento, tu navegador no soporta JavaScript</noscript>
+
+document.write: Esto imprime el texto especificado en la página. 
+console.log: Muestra un mensaje en la consola web
+alert: es una función del lenguaje de programación JavaScript que devuelve un cuadro de alerta. 
+
+string cadena="hola"
+
+var myBoolean = new Boolean ();
+x == 4 && (! (y == 1) || z == 0) 
+
+
+
+ 
